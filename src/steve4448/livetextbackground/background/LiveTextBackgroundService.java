916a1279c34dd2a54af72adcfd69c6119ccd3ed2
Stephen Hohner <hohner13@hotmail.com>
2013-09-18 20:42:54 -0400

    Drawing a singular static string w/ rects.

--- src/steve4448/livetextbackground/background/LiveTextBackgroundService.java
+++ src/steve4448/livetextbackground/background/LiveTextBackgroundService.java
@@ -1,5 +1,10 @@
 package steve4448.livetextbackground.background;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.os.Handler;
 import android.service.wallpaper.WallpaperService;
 import android.view.SurfaceHolder;
 
@@ -9,13 +14,69 @@ public class LiveTextBackgroundService extends WallpaperService {
 	    return new LiveTextBackgroundEngine();
     }
 	
-	class LiveTextBackgroundEngine extends Engine {
-		public LiveTextBackgroundEngine() {
-			
+	private class LiveTextBackgroundEngine extends Engine {
+		private SurfaceHolder holder;
+		private Paint paint = new Paint();
+		
+		private final Handler logicHandler = new Handler();
+		private final Runnable logicRunnable = new Runnable() {
+			@Override
+			public void run() {
+				draw();
+			}
+		};
+		private boolean handlerBusy = false;
+		
+		private LiveTextBackgroundEngine() {
+			paint.setAntiAlias(true);
+			paint.setStyle(Paint.Style.FILL);
+			//setupLogicHandler(true);
 		}
 		
-		public void onCreate(SurfaceHolder surface) {
-			super.onCreate(surface);
+		@Override
+		public void onSurfaceCreated(SurfaceHolder holder) {
+			this.holder = holder;
+			setupLogicHandler(true);
+		}
+		
+		@Override
+		public void onSurfaceDestroyed(SurfaceHolder holder) {
+			this.holder = null;
+			setupLogicHandler(false);
+		}
+		
+		@Override
+		public void onVisibilityChanged(boolean visible) {
+			setupLogicHandler(visible);
+		}
+		
+		private void setupLogicHandler(boolean on) {
+			if(!handlerBusy && on) {
+				logicHandler.post(logicRunnable);
+				handlerBusy = true;
+			} else if(!on) {
+				logicHandler.removeCallbacks(logicRunnable);
+				handlerBusy = false;
+			}
+		}
+		
+		private void draw() {
+			if(holder == null)
+				return;
+			System.out.println("Drawing...");
+			Canvas canvas = holder.lockCanvas();
+			try {
+				canvas.drawColor(Color.DKGRAY);
+				paint.setColor(Color.WHITE);
+				for(int i = 0; i < 3; i++)
+					canvas.drawRect(new Rect((int)(Math.random() * getDesiredMinimumHeight()), (int)(Math.random() * getDesiredMinimumHeight()), (int)(Math.random() * getDesiredMinimumHeight()), (int)(Math.random() * getDesiredMinimumHeight())), paint);
+				paint.setColor(Color.BLACK);
+				paint.setTextSize(50);
+				canvas.drawText("LEL TESTING LEL", 0, 200, paint);
+			} finally {
+				holder.unlockCanvasAndPost(canvas);
+			}
+			logicHandler.postDelayed(logicRunnable, 1000);
 		}
 	}
 }
