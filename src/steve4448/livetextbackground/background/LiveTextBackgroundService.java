b6d4142ea00dc2505dd3295420baecec2592d2e3
Stephen Hohner <hohner13@hotmail.com>
2013-09-18 21:14:42 -0400

    Simple static-content, random sized/color text falling.

--- src/steve4448/livetextbackground/background/LiveTextBackgroundService.java
+++ src/steve4448/livetextbackground/background/LiveTextBackgroundService.java
@@ -1,14 +1,16 @@
 package steve4448.livetextbackground.background;
 
+import steve4448.livetextbackground.background.object.TextObject;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Rect;
 import android.os.Handler;
 import android.service.wallpaper.WallpaperService;
 import android.view.SurfaceHolder;
 
 public class LiveTextBackgroundService extends WallpaperService {
+	public static final int DESIRED_FPS = 1000/30;
+	
 	@Override
     public Engine onCreateEngine() {
 	    return new LiveTextBackgroundEngine();
@@ -17,7 +19,7 @@ public class LiveTextBackgroundService extends WallpaperService {
 	private class LiveTextBackgroundEngine extends Engine {
 		private SurfaceHolder holder;
 		private Paint paint = new Paint();
-		
+		private TextObject[] textObj;
 		private final Handler logicHandler = new Handler();
 		private final Runnable logicRunnable = new Runnable() {
 			@Override
@@ -30,6 +32,7 @@ public class LiveTextBackgroundService extends WallpaperService {
 		private LiveTextBackgroundEngine() {
 			paint.setAntiAlias(true);
 			paint.setStyle(Paint.Style.FILL);
+			textObj = new TextObject[100];
 			//setupLogicHandler(true);
 		}
 		
@@ -66,17 +69,31 @@ public class LiveTextBackgroundService extends WallpaperService {
 			System.out.println("Drawing...");
 			Canvas canvas = holder.lockCanvas();
 			try {
+				if((int)(Math.random() * 25) == 1)
+					for(int i = 0; i < textObj.length; i++)
+						if(textObj[i] == null) {
+							textObj[i] = new TextObject("Testing", (Math.random() * getDesiredMinimumWidth()), 0, (int)(8 + Math.random() * 12), 0x00000055 + (int)(Math.random() * 0xFFFFFF55));
+							break;
+						}
 				canvas.drawColor(Color.DKGRAY);
-				paint.setColor(Color.WHITE);
-				for(int i = 0; i < 3; i++)
-					canvas.drawRect(new Rect((int)(Math.random() * getDesiredMinimumHeight()), (int)(Math.random() * getDesiredMinimumHeight()), (int)(Math.random() * getDesiredMinimumHeight()), (int)(Math.random() * getDesiredMinimumHeight())), paint);
-				paint.setColor(Color.BLACK);
-				paint.setTextSize(50);
-				canvas.drawText("LEL TESTING LEL", 0, 200, paint);
+				for(int i = 0; i < textObj.length; i++) {
+					if(textObj[i] == null)
+						continue;
+					//Text Object Logic
+					textObj[i].y += (textObj[i].velocityY+=0.1);
+					
+					//Text Object Drawing
+					paint.setColor(textObj[i].color);
+					paint.setTextSize(textObj[i].size);
+					canvas.drawText(textObj[i].text, (int)textObj[i].x, (int)textObj[i].y, paint);
+					if(textObj[i].y > getDesiredMinimumHeight() + (textObj[i].size * 2))
+						textObj[i] = null;
+						
+				}
 			} finally {
 				holder.unlockCanvasAndPost(canvas);
 			}
-			logicHandler.postDelayed(logicRunnable, 1000);
+			logicHandler.postDelayed(logicRunnable, DESIRED_FPS);
 		}
 	}
 }
---
+++ src/steve4448/livetextbackground/background/object/TextObject.java
@@ -0,0 +1,22 @@
+package steve4448.livetextbackground.background.object;
+
+public class TextObject {
+	public String text;
+	public double x;
+	public double y;
+	public double velocityX;
+	public double velocityY;
+	public int size;
+	public int color;
+	
+	/**
+	 * Used as a stub to hold the applicable variables.
+	 **/
+	public TextObject(String text, double x, double y, int size, int color) {
+		this.text = text;
+		this.x = x;
+		this.y = y;
+		this.size = size;
+		this.color = color;
+	}
+}
