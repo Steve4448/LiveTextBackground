e045325801b3b7c27339cf9fed57ac30ec22868d
Stephen Hohner <hohner13@hotmail.com>
2013-09-28 11:19:46 -0400

    Updated Rect with new helper methods from RectF.

--- src/steve4448/livetextbackground/util/Rect.java
+++ src/steve4448/livetextbackground/util/Rect.java
@@ -30,6 +30,14 @@ public class Rect {
 		return with.x >= x && with.x <= x + width && with.y >= y && with.y <= y + height;
 	}
 	
+	public static boolean intersects(Point first, Rect second) {
+		return first.x >= second.x && first.x <= second.x + second.width && first.y >= second.y && first.y <= second.y + second.height;
+	}
+	
+	public static boolean intersects(Point first, android.graphics.Rect second) {
+		return first.x >= second.left && first.x <= second.right && first.y >= second.top && first.y <= second.bottom;
+	}
+	
 	public static boolean intersects(Rect first, Rect second) {
 		return first.x >= second.x && first.x <= second.x + second.width && first.y >= second.y && first.y <= second.y + second.height;
 	}
