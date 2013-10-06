567793e55b7c51b63510a0e2d962eb1dd737848b
Steve4448 <hohner13@hotmail.com>
2013-10-06 14:34:26 -0400

    Show a toast for unfinished dialogues.

--- src/steve4448/livetextbackground/preference/ColorPickerPreference.java
+++ src/steve4448/livetextbackground/preference/ColorPickerPreference.java
@@ -4,10 +4,18 @@ import steve4448.livetextbackground.R;
 import android.content.Context;
 import android.preference.DialogPreference;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Toast;
 
 public class ColorPickerPreference extends DialogPreference {
 	public ColorPickerPreference(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		setDialogLayoutResource(R.layout.colorpickerpreference);
+	}
+	
+	@Override
+	public void onBindDialogView(View view) {
+		super.onBindDialogView(view);
+		Toast.makeText(getContext(), R.string.placeholder_not_yet_implemented, Toast.LENGTH_SHORT).show();
 	}
 }
--- src/steve4448/livetextbackground/preference/ImageModePreviewPreference.java
+++ src/steve4448/livetextbackground/preference/ImageModePreviewPreference.java
@@ -4,10 +4,18 @@ import steve4448.livetextbackground.R;
 import android.content.Context;
 import android.preference.DialogPreference;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Toast;
 
 public class ImageModePreviewPreference extends DialogPreference {
 	public ImageModePreviewPreference(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		setDialogLayoutResource(R.layout.imagemodepreviewpreference);
+	}
+	
+	@Override
+	public void onBindDialogView(View view) {
+		super.onBindDialogView(view);
+		Toast.makeText(getContext(), R.string.placeholder_not_yet_implemented, Toast.LENGTH_SHORT).show();
 	}
 }
