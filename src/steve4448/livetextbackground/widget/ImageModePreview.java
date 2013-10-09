059058456cfd566433ea64eb2b82585c4412bfef
Steve4448 <hohner13@hotmail.com>
2013-10-09 15:55:31 -0400

    Load & display image if possible

--- src/steve4448/livetextbackground/widget/ImageModePreview.java
+++ src/steve4448/livetextbackground/widget/ImageModePreview.java
@@ -1,11 +1,18 @@
 package steve4448.livetextbackground.widget;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class ImageModePreview extends View {
 	private String imageLocation;
+	private Bitmap image;
+	private Paint painter;
 	
 	public ImageModePreview(Context context, AttributeSet attrs) {
 		super(context, attrs);
@@ -18,14 +25,29 @@ public class ImageModePreview extends View {
 	}
 	
 	public void init(Context context, AttributeSet attrs, int defStyle) {
-		
+		painter = new Paint();
 	}
 	
 	public void setImageLocation(String imageLocation) {
 		this.imageLocation = imageLocation;
+		try {
+	        image = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), Uri.parse(imageLocation));
+        } catch(Exception e) {
+	        e.printStackTrace();
+        }
+		invalidate();
 	}
 	
 	public String getImageLocation() {
 		return imageLocation;
+	}
+	
+	@Override
+	public void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+		if(image == null)
+				;//Todo: Draw an image of which indicates the image failed to load?
+		else
+			canvas.drawBitmap(image, 0, 0, painter);
 	}
 }
